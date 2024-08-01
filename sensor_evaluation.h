8ca74605a1305f164f89164f84d280af04cd8942
Wingemo <philip.wingemo@outlook.com>
2024-08-01 17:51:54 +0200

    Create sensor_evaluation.h

---
+++ sensor_evaluation.h
@@ -0,0 +1,9 @@
+// sensor_evaluation.h
+#ifndef SENSOR_EVALUATION_H
+#define SENSOR_EVALUATION_H
+
+#include "sensor_interface.h"
+
+int evaluateSensorInput(SensorData data);
+
+#endif // SENSOR_EVALUATION_H
