273cba35a3c90e837822481bc7b3ae90d9693a7a
Wingemo <philip.wingemo@outlook.com>
2024-08-01 17:49:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>  // För sleep()
-#include <wiringPi.h>
-
-typedef struct {
-    int tempPin;
-    int pressPin;
-    int humidPin;
-    int outputPin;
-} PinConfig;
-
-typedef struct {
-    int tempReading;
-    int pressReading;
-    int humidReading;
-} SensorData;
+#include <unistd.h>  // For sleep()
+#include "sensor_interface.h"
 
 const int SENSOR_THRESHOLD = 2;
 
-int initialize(PinConfig pins) {
+int initialize(PinConfig *pins) {
     return initializePins(pins);
-}
-
-SensorData readSensors(PinConfig pins) {
-    SensorData data = {0, 0, 0};
-    data.tempReading = digitalRead(pins.tempPin);
-    data.pressReading = digitalRead(pins.pressPin);
-    data.humidReading = digitalRead(pins.humidPin);
-    return data;
 }
 
 int evaluateSensorInput(SensorData data) {
@@ -36,25 +14,12 @@ int evaluateSensorInput(SensorData data) {
     return activeSensorCount >= SENSOR_THRESHOLD;
 }
 
-int sendOutput(PinConfig pins, int result) {
-    digitalWrite(pins.outputPin, result ? HIGH : LOW);
-    return 0;
-}
-
-int execute(PinConfig pins) {
+int execute(PinConfig *pins) {
     SensorData data = readSensors(pins);
     if (data.tempReading == -1 || data.pressReading == -1 || data.humidReading == -1) {
-        fprintf(stderr, "Failed to read sensor data\n");
         return -1;
     }
-
-    int result = evaluateSensorInput(data);
-    if (sendOutput(pins, result) != 0) {
-        fprintf(stderr, "Failed to send output\n");
-        return -1;
-    }
-
-    return 0;
+    return sendOutput(pins, evaluateSensorInput(data));
 }
 
 int main() {
@@ -65,12 +30,11 @@ int main() {
         .outputPin = 3
     };
 
-    if (initialize(pins) != 0) {
+    if (initialize(&pins) != 0) {
         return 1;
     }
 
-    if (execute(pins) != 0) {
-        fprintf(stderr, "Execution failed\n");
+    if (execute(&pins) != 0) {
         return 1;
     }
 
